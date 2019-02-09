9692d6c2f6e1d00514c473be1576144792d2114a
Roger Spielmann <drummer23@braveheart.ch>
2019-02-09 14:53:32 +0700

    Adds Fetching products

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -31,4 +31,27 @@ class ProductController extends AbstractController
 
       return new Response('Saved new product with id '.$product->getId());
     }
+
+
+    /**
+     * @Route("/product/{id}", name="product_show")
+     */
+    public function show($id)
+    {
+        $product = $this->getDoctrine()
+            ->getRepository(Product::class)
+            ->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        return new Response('Check out this great product: '.$product->getName());
+
+        // or render a template
+        // in the template, print things with {{ product.name }}
+        // return $this->render('product/show.html.twig', ['product' => $product]);
+    }
 }
