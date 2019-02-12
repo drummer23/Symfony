be3d5d1e99110408361268b23e53e6200e905844
Roger Spielmann <drummer23@braveheart.ch>
2019-02-12 14:32:10 +0700

    adds logger

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+use Psr\Log\LoggerInterface;
 
 
 class LuckyController extends AbstractController
@@ -12,18 +15,22 @@ class LuckyController extends AbstractController
     /**
      * @Route("/lucky", name="lucky")
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('lucky/index.html.twig', [
-            'controller_name' => 'LuckyController',
-        ]);
+
+
+      return $this->render('lucky/index.html.twig', [
+          'controller_name' => 'LuckyController',
+      ]);
     }
 
     /**
      * @Route("/lucky/number", name="number")
      */
-    public function number()
+    public function number(LoggerInterface $logger)
     {
+      $logger->info('we are logging');
+
       $number = random_int(0,100);
 
       return $this->render('lucky/number.html.twig', [
