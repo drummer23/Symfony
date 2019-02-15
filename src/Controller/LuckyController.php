0aa4c8854fd01ec62f92ceaa32d19cf619b6976c
Roger Spielmann <drummer23@braveheart.ch>
2019-02-15 16:09:38 +0700

    adds basic session example

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -7,6 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Psr\Log\LoggerInterface;
 
 
@@ -15,13 +16,32 @@ class LuckyController extends AbstractController
     /**
      * @Route("/lucky", name="lucky")
      */
-    public function index(Request $request)
+    public function index(LoggerInterface $logger, Request $request)
     {
+
+      $page = $request->query->get('page', 1);
+
+      $logger->info("page is " . $page);
 
 
       return $this->render('lucky/index.html.twig', [
           'controller_name' => 'LuckyController',
       ]);
+    }
+
+    /**
+     * @Route("/lucky/session", name="session")
+     */
+    public function session(LoggerInterface $logger, SessionInterface $session)
+    {
+      $counter = $session->get('counter');
+
+      $counter++;
+
+      $session->set('counter', $counter);
+
+      return new Response('Reload Counter is '.$counter);
+
     }
 
     /**
